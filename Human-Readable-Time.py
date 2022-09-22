2c2e44b9f6d999a7d886e19ee540bcc15d614e12
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-22 07:47:58 +0200

    second >= 3600

---
+++ Human-Readable-Time.py
@@ -0,0 +1,19 @@
+def make_readable(seconds):
+
+	""" This function takes a non-negative integer (seconds) 
+	as input and returns the time in a human-readable format 
+	(HH:MM:SS)"""
+	if seconds > 0 and seconds >= 3600:
+		HH = int(seconds / 3600)
+		MM = int((seconds % 3600) / 60)
+		SS = int(((seconds % 3600) / 60)%60)
+		Human_readable = f"{HH}:{MM}:{SS}"
+		return(Human_readable)
+	elif seconds in range(1, 3600):
+		HH = int(seconds / 3600)
+		MM = int((seconds % 3600) / 60)
+		SS = int(((seconds % 3600) / 60)%60)
+		Human_readable = f"{HH}:{MM}:{SS}"
+		return(Human_readable)
+	else:
+		print("seconds are less than 0")

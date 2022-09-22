1a18c77a4ec47d73fdbfcf4fdb32789294fe2405
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-22 09:20:24 +0200

    introduction of zfill

--- Human-Readable-Time.py
+++ Human-Readable-Time.py
@@ -3,31 +3,35 @@ def make_readable(seconds):
 	""" This function takes a non-negative integer (seconds) 
 	as input and returns the time in a human-readable format 
 	(HH:MM:SS)"""
-	if seconds > 0 and seconds >= 3600:
+	
+	if seconds > 3600:
 		HH = int(seconds / 3600)
 		MM = int((seconds % 3600) / 60)
 		SS = int(((seconds % 3600) / 60)%60)
-		Human_readable = f"{HH}:{MM}:{SS}"
+		if HH < 10 or MM < 10 or SS < 10:
+		    Human_readable = f"{str(HH).zfill(2)}:{str(MM).zfill(2)}:{str(SS).zfill(2)}"
+		else:
+		    Human_readable = f"{HH}:{MM}:{SS}"
 		return(Human_readable)
 
-	elif seconds in range(1, 3600):
-		HH = str(00)
+	elif seconds >= 60 and seconds <= 3600:
+		HH = 0
 		MM = int(seconds / 60)
-		SS = int((seconds / 60)%60)
-		Human_readable = f"{HH}:{MM}:{SS}"
+		SS = int(seconds % 60)
+		if MM < 10 or SS < 10:
+		    Human_readable = f"00:{str(MM).zfill(2)}:{str(SS).zfill(2)}"
+		else:
+		    Human_readable = f"00:{MM}:{SS}"
 		return(Human_readable)
 
-	elif seconds in range(1, 60):
-		HH = str(00)
-		MM = str(00)
-		SS = seconds % 60
-		Human_readable = f"{HH}:{MM}:{SS}"
+	elif seconds > 0 and seconds <= 60:
+		SS = seconds
+		if SS < 10:
+		    Human_readable = f"00:00:{str(SS).zfill(2)}"
+		else:
+		    Human_readable = f"00:00:{SS}"
 		return(Human_readable)
 		
 	else:
-		HH = str(00)
-		MM = str(00)
-		SS = str(00)
-		Human_readable = f"{HH}:{MM}:{SS}"
+		Human_readable = "00:00:00"
 		return(Human_readable)
-

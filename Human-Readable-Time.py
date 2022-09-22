3aa2b65c552a75300c9ba0abb044c8210beb5fda
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-22 09:32:28 +0200

    function comment

--- Human-Readable-Time.py
+++ Human-Readable-Time.py
@@ -1,4 +1,9 @@
 def make_readable(seconds):
+
+	""" This function takes a non-negative integer (seconds) 
+	as input and returns the time in a human-readable format 
+	(HH:MM:SS)"""
+	
     h = seconds // 3600
     m = (seconds - h * 3600) // 60
     s = seconds - (h * 3600) - (m * 60)

63aa11f40312a5f33bd8acc9cbf2f3ad60d5bb13
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-22 08:05:10 +0200

    second in range(1- 3600)

--- Human-Readable-Time.py
+++ Human-Readable-Time.py
@@ -10,9 +10,16 @@ def make_readable(seconds):
 		Human_readable = f"{HH}:{MM}:{SS}"
 		return(Human_readable)
 	elif seconds in range(1, 3600):
-		HH = int(seconds / 3600)
-		MM = int((seconds % 3600) / 60)
-		SS = int(((seconds % 3600) / 60)%60)
+		HH = str(00)
+		MM = int(seconds / 60)
+		SS = int((seconds / 60)%60)
+		Human_readable = f"{HH}:{MM}:{SS}"
+		return(Human_readable)
+
+	elif seconds in range(1, 60):
+		HH = str(00)
+		MM = str(00)
+		SS = seconds % 60
 		Human_readable = f"{HH}:{MM}:{SS}"
 		return(Human_readable)
 	else:

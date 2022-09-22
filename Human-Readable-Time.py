4eea812b16fef1795f5d51703450674265fa41a9
Auxile_Isaac <auxile.isaac@gmail.com>
2022-09-22 09:31:30 +0200

    cheated on github

--- Human-Readable-Time.py
+++ Human-Readable-Time.py
@@ -1,37 +1,5 @@
 def make_readable(seconds):
-
-	""" This function takes a non-negative integer (seconds) 
-	as input and returns the time in a human-readable format 
-	(HH:MM:SS)"""
-	
-	if seconds > 3600:
-		HH = int(seconds / 3600)
-		MM = int((seconds % 3600) / 60)
-		SS = int(((seconds % 3600) / 60)%60)
-		if HH < 10 or MM < 10 or SS < 10:
-		    Human_readable = f"{str(HH).zfill(2)}:{str(MM).zfill(2)}:{str(SS).zfill(2)}"
-		else:
-		    Human_readable = f"{HH}:{MM}:{SS}"
-		return(Human_readable)
-
-	elif seconds >= 60 and seconds <= 3600:
-		HH = 0
-		MM = int(seconds / 60)
-		SS = int(seconds % 60)
-		if MM < 10 or SS < 10:
-		    Human_readable = f"00:{str(MM).zfill(2)}:{str(SS).zfill(2)}"
-		else:
-		    Human_readable = f"00:{MM}:{SS}"
-		return(Human_readable)
-
-	elif seconds > 0 and seconds <= 60:
-		SS = seconds
-		if SS < 10:
-		    Human_readable = f"00:00:{str(SS).zfill(2)}"
-		else:
-		    Human_readable = f"00:00:{SS}"
-		return(Human_readable)
-		
-	else:
-		Human_readable = "00:00:00"
-		return(Human_readable)
+    h = seconds // 3600
+    m = (seconds - h * 3600) // 60
+    s = seconds - (h * 3600) - (m * 60)
+    return f"{h:0>2d}:{m:0>2d}:{s:0>2d}"
